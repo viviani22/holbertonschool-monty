492138b78108bd953b6ed01c558aed92e037312c
viviani22 <vivianxhumetiku@gmail.com>
2022-08-15 02:46:37 -0700

    temp

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * main - entry point
+ * @argc: argument number
+ * @argv: array of arguments
+ * Return: 0 is successful
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 1)
+		perror("USAGE: monty file");
+}
